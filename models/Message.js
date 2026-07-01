2be2ba55bec24730ab969bddc2b1e7afea7a9398
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-07-01 21:05:46 +0530

    Schema for individual chat messages in a conversation

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+
+
+// Schema for individual chat messages in a conversation
+const MessageSchema = new mongoose.Schema({
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true
+  },
