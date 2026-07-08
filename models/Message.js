ec60766c50eda5e0cac2b7970b6e17d7d23e70b0
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-07-08 22:48:15 +0530

    .

--- models/Message.js
+++ models/Message.js
@@ -5,6 +5,11 @@ const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
   conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true
+  },
+
     
 
   sender: {
