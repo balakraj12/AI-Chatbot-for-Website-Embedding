e5070f0a28ec37a0001fc90d9efb218dd33211ed
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-07-08 22:46:57 +0530

    remove some codes from schema

--- models/Message.js
+++ models/Message.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 
 
 // Schema for individual chat messages in a conversation
-const MessageSchema = new mongoose.Schema({
-  conversationId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Conversation',
-    required: true
-  },
+
 
   sender: {
     type: String,
