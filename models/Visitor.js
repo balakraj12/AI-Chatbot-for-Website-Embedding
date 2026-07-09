d6dade917e927093230197d4b147d58d90b980ae
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-07-09 23:58:58 +0530

    name updated

--- models/Visitor.js
+++ models/Visitor.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 // Schema for tracking chatbot visitors/users and their context
 const VisitorSchema = new mongoose.Schema({
 
-     name: {
-    type: String,
-    required: true,
-    trim: true
-    },
+    
   profession: {
     type: String,
     required: true,
