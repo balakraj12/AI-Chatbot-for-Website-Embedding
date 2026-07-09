d7be85a7b934b056133e88e7b5e1cd55253258bd
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-07-09 23:57:44 +0530

    .

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// Schema for tracking a conversation session associated with a visitor
+
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'Visitor',
     required: true
   },
