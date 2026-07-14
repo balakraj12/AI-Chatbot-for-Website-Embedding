8c6c4ce6345fe849138db6f4353722930c38e3d8
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-07-14 20:19:41 +0530

    Parse incoming JSON payloads updated

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(helmet({
 app.use(cors());
 
 // Parse incoming JSON payloads
-// app.use(express.json());
+app.use(express.json());
 
 // Set up API rate limiting to protect backend resources
 const apiLimiter = rateLimit({
